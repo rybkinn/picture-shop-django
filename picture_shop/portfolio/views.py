2fa6dede2cdfab6333609c006959e6ddd5c40959
Nikita <4rybkin@gmail.com>
2022-06-02 20:10:27 +0300

    added main,about-me,contacts site pages

--- picture_shop/portfolio/views.py
+++ picture_shop/portfolio/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'portfolio/index.html')
+
+
+def about_me(request):
+    return render(request, 'portfolio/about-me.html')
+
+
+def contacts(request):
+    return render(request, 'portfolio/contacts.html')
