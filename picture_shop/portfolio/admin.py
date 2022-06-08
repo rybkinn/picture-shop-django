17943e2b8e466f894ccd24b139393f7637fbcd0d
Nikita Rybkin(wsl) <4rybkin@gmail.com>
2022-06-08 20:04:03 +0300

    added portfolio app in admin panel

--- picture_shop/portfolio/admin.py
+++ picture_shop/portfolio/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import *
+
+
+class MyWorkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'picture', 'is_published', 'get_picture')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description')
+    list_filter = ('is_published',)
+
+    def get_picture(self, obj):
+        if obj.picture:
+            return mark_safe(f'<img src="{obj.picture.url}" width="75">')
+        else:
+            return '-'
+
+    get_picture.short_description = 'Миниатюра'
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+
+
+class MyClientsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'working_position', 'photo', 'facebook', 'twitter',
+                    'instagram', 'comment', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(MyWork, MyWorkAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(MyClient, MyClientsAdmin)
+
