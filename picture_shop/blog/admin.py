c9636b25e74cc5ca90ac363e7e03c3e83f953463
Nikita Rybkin(wsl) <4rybkin@gmail.com>
2022-06-10 17:37:30 +0300

    added blog to admin panel

--- picture_shop/blog/admin.py
+++ picture_shop/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'background_image', 'post_time')
