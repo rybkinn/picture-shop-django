3108ffbee715c35b8c04188cae15dfdd51d24f1c
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-06-21 20:34:47 +0300

    deleted comment

--- picture_shop/blog/templatetags/sidebar.py
+++ picture_shop/blog/templatetags/sidebar.py
@@ -16,9 +16,3 @@ def get_archive():
 @register.inclusion_tag('blog/widgets/gallery.html')
 def get_gallery():
     pass
-
-
-# @register.inclusion_tag('blog/popular_posts_tpl.html')
-# def get_popular(cnt=3):
-#     posts = Post.objects.order_by('-views')[:cnt]
-#     return {'posts': posts}
