0c67f131d301e8c86bddcaff9ee188763ed03b6f
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-06-29 20:44:35 +0300

    added function to add blog posts

--- picture_shop/blog/views.py
+++ picture_shop/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.views.generic import DetailView, ListView
 
 from users.models import CustomUser
@@ -9,15 +10,33 @@ class PostBlog(ListView):
     template_name = 'blog/blog.html'
     context_object_name = 'posts'
 
+    start_posts_number = 4
+    posts_left = Post.objects.count() - start_posts_number
+    current_number_posts = start_posts_number
+
+    count_posts_add = 2
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Блог'
         context['users'] = CustomUser.objects.all()
+        context['posts_left'] = self.posts_left
         return context
 
     def get_queryset(self):
-        post_per_page = 4
-        return Post.objects.all().order_by('-pk')[:post_per_page]
+        return Post.objects.all().order_by('-pk')[:self.start_posts_number]
+
+    @staticmethod
+    def show_more_posts(request):
+        current_posts = Post.objects.all().order_by('-pk')[
+                        PostBlog.current_number_posts:PostBlog.current_number_posts+PostBlog.count_posts_add]
+
+        PostBlog.current_number_posts += PostBlog.count_posts_add
+        PostBlog.posts_left -= PostBlog.count_posts_add
+
+        json_data = list(current_posts.values())
+        json_data.insert(0, {'total_posts_count': PostBlog.posts_left})
+        return JsonResponse(json_data, safe=False)
 
 
 class GetSinglePost(DetailView):
