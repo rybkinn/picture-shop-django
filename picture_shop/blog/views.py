bf9f06e12e60c41ff4b9ffdc3ff69d86d72a701c
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-06-30 20:54:00 +0300

    added display when adding posts

--- picture_shop/blog/views.py
+++ picture_shop/blog/views.py
@@ -1,8 +1,11 @@
+from pprint import pprint
+
+from datetime import datetime, timezone
 from django.http import JsonResponse
 from django.views.generic import DetailView, ListView
 
 from users.models import CustomUser
-from .models import Post
+from .models import Post, Category
 
 
 class PostBlog(ListView):
@@ -33,9 +36,19 @@ class PostBlog(ListView):
 
         PostBlog.current_number_posts += PostBlog.count_posts_add
         PostBlog.posts_left -= PostBlog.count_posts_add
-
+        a = Post.objects.get(id=24).post_time
+        pprint(a.replace(tzinfo=timezone.utc))
         json_data = list(current_posts.values())
+        for content in json_data:
+            content['author'] = CustomUser.objects.get(id=content['author_id']).username
+            content['author_avatar'] = '/media/' + str(CustomUser.objects.get(id=content['author_id']).avatar)
+            content['background_image'] = '/media/' + content['background_image']
+            # content['post_time'] = Post.objects.get(id=content['id']).post_time.tzinfo
+            del content['author_id']
+            del content['category_id']
+
         json_data.insert(0, {'total_posts_count': PostBlog.posts_left})
+
         return JsonResponse(json_data, safe=False)
 
 
