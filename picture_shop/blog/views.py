b51f90f81ab766c493200f8ce6b13ec31f87c34b
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-07-01 21:34:18 +0300

    completed work with added posts

--- picture_shop/blog/views.py
+++ picture_shop/blog/views.py
@@ -1,11 +1,10 @@
-from pprint import pprint
-
-from datetime import datetime, timezone
+from django.conf import settings
 from django.http import JsonResponse
+from django.utils import dateformat
 from django.views.generic import DetailView, ListView
 
 from users.models import CustomUser
-from .models import Post, Category
+from .models import Post
 
 
 class PostBlog(ListView):
@@ -14,16 +13,12 @@ class PostBlog(ListView):
     context_object_name = 'posts'
 
     start_posts_number = 4
-    posts_left = Post.objects.count() - start_posts_number
-    current_number_posts = start_posts_number
-
     count_posts_add = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Блог'
         context['users'] = CustomUser.objects.all()
-        context['posts_left'] = self.posts_left
         return context
 
     def get_queryset(self):
@@ -31,23 +26,25 @@ class PostBlog(ListView):
 
     @staticmethod
     def show_more_posts(request):
-        current_posts = Post.objects.all().order_by('-pk')[
-                        PostBlog.current_number_posts:PostBlog.current_number_posts+PostBlog.count_posts_add]
+        count_posts_displayed = int(request.GET.get('count_posts', default=0))
+        posts_left = Post.objects.count() - count_posts_displayed
 
-        PostBlog.current_number_posts += PostBlog.count_posts_add
-        PostBlog.posts_left -= PostBlog.count_posts_add
-        a = Post.objects.get(id=24).post_time
-        pprint(a.replace(tzinfo=timezone.utc))
-        json_data = list(current_posts.values())
+        sent_posts = Post.objects.all().order_by('-pk')[
+                     count_posts_displayed:count_posts_displayed + PostBlog.count_posts_add]
+
+        json_data = list(sent_posts.values())
         for content in json_data:
             content['author'] = CustomUser.objects.get(id=content['author_id']).username
             content['author_avatar'] = '/media/' + str(CustomUser.objects.get(id=content['author_id']).avatar)
             content['background_image'] = '/media/' + content['background_image']
-            # content['post_time'] = Post.objects.get(id=content['id']).post_time.tzinfo
+            content['description'] = ' '.join(str(content['description']).split(' ')[:20]) + ' …'
+            content['post_time'] = dateformat.format(
+                Post.objects.get(id=content['id']).post_time, settings.DATE_FORMAT + ' г. H:i')
             del content['author_id']
             del content['category_id']
 
-        json_data.insert(0, {'total_posts_count': PostBlog.posts_left})
+        json_data.insert(0, {'posts_left': posts_left,
+                             'count_posts_add': PostBlog.count_posts_add})
 
         return JsonResponse(json_data, safe=False)
 
