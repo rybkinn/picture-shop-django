6ed19249119ea745acf4f4bd414508d71237e857
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-07-11 21:58:20 +0300

    added generate archives list in template

--- picture_shop/blog/templatetags/sidebar.py
+++ picture_shop/blog/templatetags/sidebar.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils import dateformat
+
+from blog.models import Post
 
 register = template.Library()
 
@@ -10,7 +13,12 @@ def get_search():
 
 @register.inclusion_tag('blog/widgets/archive.html')
 def get_archive():
-    pass
+    archived_posts = Post.objects.filter(is_archived=True).order_by('creation_time')
+    dates_archive_list = [dateformat.format(post.creation_time, "F Y") for post in archived_posts]
+
+    dates_archive_list[:] = list(dict.fromkeys(dates_archive_list))
+
+    return {'dates_archive_list': dates_archive_list}
 
 
 @register.inclusion_tag('blog/widgets/gallery.html')
