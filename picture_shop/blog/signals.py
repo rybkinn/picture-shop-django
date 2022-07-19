34a8abc3d37329a8470479e6d0c4d622ef0dac11
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-07-19 18:14:57 +0300

    added delete pictures in blog gallery signal

--- picture_shop/blog/signals.py
+++ picture_shop/blog/signals.py
@@ -29,3 +29,19 @@ def create_mini_picture_in_blog_gallery(instance, created, **kwargs):
             else:
                 img.save(instance.thumbnail.storage.base_location + '/' + file_name_finish)
             Gallery.objects.filter(image=instance.image.name).update(thumbnail=file_name_finish)
+
+
+@receiver(post_delete, sender=Gallery)
+def delete_pictures_in_blog_gallery(instance, **kwargs):
+    """
+    Deletes the selected picture and its mini counterpart.
+    """
+    if instance.image.name:
+        instance.image.delete(False)
+    else:
+        raise FileNotFoundError("Not file image.")
+
+    if instance.thumbnail.name:
+        instance.thumbnail.delete(False)
+    else:
+        raise FileNotFoundError('Not file mini image.')
