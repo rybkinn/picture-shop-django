2ece1c50d91c7dc5d53eae184af0e64b654261df
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-07-19 17:05:47 +0300

    signal file created and save method moved from model

--- picture_shop/blog/models.py
+++ picture_shop/blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-from PIL import Image
-
 
 class Category(models.Model):
     title = models.CharField(max_length=255, unique=True, verbose_name='Название')
@@ -49,29 +47,9 @@ class Gallery(models.Model):
     image = models.ImageField(upload_to='blog_gallery/%Y/%m/%d', verbose_name='Картинка')
     thumbnail = models.ImageField(upload_to='blog_gallery_mini/%Y/%m/%d', null=True, blank=True, verbose_name='Миниатюра')
 
-    def save(self, *args, **kwargs):
-        super(Gallery, self).save(*args, **kwargs)
-        img = Image.open(self.image.path)
-        if img.width > 160 or img.height > 160:
-            output_size = (160, 160)
-            img.thumbnail(output_size)
-            length_varchar_field = 100
-            file_name = '.'.join(self.image.name.split('.')[:-1])
-            file_type = '.' + self.image.name.split('.')[-1]
-            file_name_finish = file_name + "_mini" + file_type
-            if len(file_name_finish) > length_varchar_field:
-                number_unnecessary_letters = len(file_name_finish) - length_varchar_field
-                file_path_sold = file_name.split('_')[-1]
-                file_name = '_'.join(file_name.split('_')[:-1])[:-number_unnecessary_letters] + '_' + file_path_sold
-                file_name_finish = file_name + "_mini" + file_type
-                img.save(self.thumbnail.storage.base_location + '/' + file_name_finish)
-            else:
-                img.save(self.thumbnail.storage.base_location + '/' + file_name_finish)
-            Gallery.objects.filter(image=self.image.name).update(thumbnail=file_name_finish)
-
-    def __str__(self):
-        return self.image.name.split('/')[-1]
-
     class Meta:
         verbose_name = 'Картинку'
         verbose_name_plural = 'Галерея'
+
+    def __str__(self):
+        return self.image.name.split('/')[-1]
---
+++ picture_shop/blog/signals.py
@@ -0,0 +1,31 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch.dispatcher import receiver
+
+from PIL import Image
+
+from .models import *
+
+
+@receiver(post_save, sender=Gallery)
+def create_mini_picture_in_blog_gallery(instance, created, **kwargs):
+    """
+    Creates a mini picture(160x160) based on the loaded one.
+    """
+    if created:
+        img = Image.open(instance.image.path)
+        if img.width > 160 or img.height > 160:
+            output_size = (160, 160)
+            img.thumbnail(output_size)
+            length_varchar_field = 100
+            file_name = '.'.join(instance.image.name.split('.')[:-1])
+            file_type = '.' + instance.image.name.split('.')[-1]
+            file_name_finish = file_name + "_mini" + file_type
+            if len(file_name_finish) > length_varchar_field:
+                number_unnecessary_letters = len(file_name_finish) - length_varchar_field
+                file_path_sold = file_name.split('_')[-1]
+                file_name = '_'.join(file_name.split('_')[:-1])[:-number_unnecessary_letters] + '_' + file_path_sold
+                file_name_finish = file_name + "_mini" + file_type
+                img.save(instance.thumbnail.storage.base_location + '/' + file_name_finish)
+            else:
+                img.save(instance.thumbnail.storage.base_location + '/' + file_name_finish)
+            Gallery.objects.filter(image=instance.image.name).update(thumbnail=file_name_finish)
