fb32c80ed0fc9229779e7cf903e4558548f0d33d
Nikita Rybkin(Win) <4rybkin@gmail.com>
2022-07-29 19:26:46 +0300

    added page error 500

--- picture_shop/picture_shop/views.py
+++ picture_shop/picture_shop/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def page_not_found_view(request, exception, template_name='404.html'):
     return render(request, template_name, status=404)
+
+
+def page_internal_server_error(request, exception, template_name='500.html'):
+    return render(request, template_name, status=500)
